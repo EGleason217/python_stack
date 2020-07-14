5780ca42fad62d1df5f42b46db454ca433e5a03f
EGleason217 <erik@gleasoncf.com>
2020-07-13 23:53:41 -0400

    1st Commit

--- _python/python_fundamentals/Python_opp.py
+++ _python/python_fundamentals/Python_opp.py
@@ -1,6 +1,35 @@
 class User:
-    pass    # we'll fill this in shortly
+    def __init__(self, username, email_address):
+        self.name = username
+        self.email = email_address
+        self.account_balance = 0
+    def make_deposit(self, amount):
+        self.account_balance += amount
+    def make_withdrawal(self,amount):
+        self.account_balance -= amount
+    def display_user_balance(self):
+        print("User: " + self.name + " -- Account Balance: " + str(self.account_balance))
+    def transfer(self, to_account, amount):
+        to_account.make_deposit(amount)
+        self.make_withdrawal(amount)
 
-michael = User()
-anna = User()
+korine = User("Korine", "angelkorine@yahoo.com")
+korim = User("Korim", "korim_powell@outlook.com")
+davron = User("Davron", "davron_powell@outlook.com")
 
+print(korine.name)
+print(korim.name)
+
+korine.make_deposit(1000)
+print(korine.account_balance)
+
+korine.make_deposit(2000)
+print(korine.account_balance)
+
+korine.make_withdrawal(333)
+print(korine.account_balance)
+
+
+korine.transfer(korim,600)
+korine.display_user_balance()
+korim.display_user_balance()
