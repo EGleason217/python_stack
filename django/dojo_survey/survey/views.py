8c2f79ad57493287fe6a0b59ea3eb3bc6f0f9df1
EGleason217 <erik@gleasoncf.com>
2020-07-24 20:00:02 -0400

    2nd Version of Assignment

--- django/dojo_survey/survey/views.py
+++ django/dojo_survey/survey/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def survey(request):
     return render(request, "index.html")
 
 def process(request):
-    if request.method == 'POST':
-        context = {
+    if request.method == 'GET':
+        return redirect('/')
+    request.session['result'] = {
             'user_name': request.POST['user_name'],
             'language': request.POST['language'],
             'location': request.POST['location']
         }
-        return render(request, 'result.html', context)
+    return redirect('/result')
     return render(request, 'result.html')
+
+def result(request):
+    context = {
+        'result': request.session['result']
+    }
+    return render(request, 'result.html', context)
