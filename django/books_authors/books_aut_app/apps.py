fdf582121c7cac22f544335ee1775f56e54dd80d
EGleason217 <erik@gleasoncf.com>
2020-08-04 20:55:42 -0400

    latest update Aug 3 2020

---
+++ django/books_authors/books_aut_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BooksAutAppConfig(AppConfig):
+    name = 'books_aut_app'
---
+++ django/user_model/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/erikgleason/python_stack/django/user_model/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
